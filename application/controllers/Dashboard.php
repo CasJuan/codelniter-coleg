46975420a200c9f6cabf2804d7bb14be7f24dbd5
CasJuan <castrojuanig25@gmail.com>
2025-01-03 22:05:46 -0300

    [add] views del modelo

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('BASEPATH') OR exit ('No direct script access allowed');
+
+class Dashboard extends CI_Controller{
+
+
+    public function index(){
+        $this->load->view('includes/header');
+        $this->load->view('includes/sidebar');
+        $this->load->view('home');
+        $this->load->view('includes/footer');
+    }
+
+
+    public function hola(){
+        $this->Site_modal-> updateProfesor();
+    }
+
+}
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,7 +20,10 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('includes/header');
+		$this->load->view('includes/sidebar');
+		$this->load->view('home');
+		$this->load->view('includes/footer');
 	}
 
 	public function hola()
